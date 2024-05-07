cf6222b37a415f8ce8448f950c6d33d18c3300ee
Daniel Smith <danjsmith3@gmail.com>
2024-05-07 12:23:26 -0500

    1

--- src/utils/transformations.ts
+++ src/utils/transformations.ts
@@ -1,9 +1,15 @@
-export const capitalize = () => {
-    // todo: build this function
-    // `capitalize("jOn")` should output `"Jon"`
-}
+export const capitalize = (str: string) => {
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();}
 
-export const formatPhoneNumber = () => {
-    // todo: build this function
-    // `formatPhoneNumber("1234567")` should be `"12-34-56-7"`
+export const formatPhoneNumber = (phoneNumberInput) => {
+    let formatted = '';
+    for (let i = 0; i < number.length; i++) {
+        formatted += number.charAt(i);
+
+        if ((i + 1) % 2 === 0 && i < phoneNumberInput.length - 1) {
+            formatted += '-';
+        }
+    }
+
+    return formatted;
 }
